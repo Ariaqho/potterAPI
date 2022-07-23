f1d8f98e670a1b5a01250fb710b52c04d09fd2f3
Felipe Maldonado <felipe.arica@gmail.com>
2022-07-23 12:36:53 -0400

    comentario de prueba

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import NavbarComponent from './components/Navbar/Navbar';
 import Characters from './components/Characters/Characters';
 import SpellsList from './components/Spells/Spells';
 
+//Comentario de prueba 
+
 function App() {
   SpellsList()
   return (
