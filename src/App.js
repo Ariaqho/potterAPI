90420e9182d52129424a18840a0c22183bd378b0
Felipe Maldonado <felipe.arica@gmail.com>
2022-07-23 10:03:35 -0400

    cambio de rama

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar_component from './components/Navbar/Navbar';
+import Listado_hechizos from './components/Hechizos/Hechizos';
 
 function App() {
+  Listado_hechizos()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar_component />
     </div>
+    
   );
 }
+
 
 export default App;
