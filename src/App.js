77ad77019a3a33fde53f07255c79aff110857b49
FaviolaPaz <105898378+FaviolaPaz@users.noreply.github.com>
2022-07-26 23:27:51 -0400

    creado componente HOME, creada logica en el Navbar, actualizado el css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavbarComponent from './components/Navbar/Navbar';
 import Characters from './components/Characters/Characters';
 import SpellsList from './components/Spells/Spells';
 import Books from './components/Books'
+import Home from './components/Home/Home'
 
 //Comentario de prueba 
 
 function App() {
   //SpellsList()
   return (
-    <div className="App">
-      <NavbarComponent />
-      <SpellsList />  
-      <Characters />
-      <Books />
-    </div>
-    
+    // <div className="App">
+    //   <NavbarComponent />
+    //   <SpellsList />  
+    //   <Characters />
+    //   <Books />
+    // </div>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NavbarComponent />}>
+          <Route index element={<Home />} />
+          <Route path="hechizos" element={<SpellsList />} />
+          <Route path="personajes" element={<Characters />} />
+          <Route path="libros" element={<Books />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
+
+
   );
 }
 
