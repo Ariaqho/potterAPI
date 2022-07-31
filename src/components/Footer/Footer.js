4d9fca7a8a5d5fa271ed116cc5e1e9ba7f8b6f72
Felipe Maldonado <felipe.arica@gmail.com>
2022-07-31 18:41:10 -0400

    trabajando en estilos

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,28 +1,42 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHatWizard, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin, fa } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
     <footer>
-        <div className="footer__col1">
-          <h2>ICONO</h2>
-          <p>Proyecto final curso frontend asociación software libre Chile - USACH <br/>proyecto creado por:</p>
-          </div>
-          <div className="footer__col2">
-            <h4>@Faviola</h4>
-            <ul>
-              <li><a className= 'link' href='https://github.com/FaviolaPaz' target='_blank' rel='noreferrer' style={{ textDecoration: 'none'}}>Github</a></li>
-              <li><a className= 'link' href='https://google.cl' target='_blank' rel='noreferrer' style={{ textDecoration: 'none'}}>LinkedIn</a></li>
-              <li><a className= 'link' href='https://google.cl' target='_blank' rel='noreferrer' style={{ textDecoration: 'none'}}>Gmail</a></li>
-            </ul>
-          </div>
-          <div className="footer__col3">
-            <h4>@Felipe</h4>
-            <ul>
-              <li><a className= 'link' href='https://github.com/Ariaqho' target='_blank' rel='noreferrer' style={{ textDecoration: 'none'}}>Github</a></li>
-              <li><a className= 'link' href='https://google.cl' target='_blank' rel='noreferrer' style={{ textDecoration: 'none'}}>LinkedIn</a></li>
-              <li><a className= 'link' href='https://google.cl' target='_blank' rel='noreferrer' style={{ textDecoration: 'none'}}>Gmail</a></li>
-            </ul>
-          </div>
+      <div className="footer__col1">
+        <div className='hat-icon-footer'><FontAwesomeIcon icon={faHatWizard}></FontAwesomeIcon></div>
+        <div className='description-footer'>
+          <h2>Potter</h2>
+          <p>Proyecto final curso frontend asociación software libre Chile - USACH <br />proyecto creado por:</p>
+        </div>
+      </div>
+      <div className="footer__col2">
+        <h4 style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>@Faviola</h4>
+        <div className='contact__icons-1 d-flex justify-content-evenly'>
+          <a className='link' href='https://github.com/FaviolaPaz' target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon></a>
+          <a className='link' href='https://linkedin.com/in/faviola-paz' target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon></a>
+          <a className='link' href='mailto:faviola4@gmail.com' target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon></a>
+        </div>
+      </div>
+      <div className="footer__col3">
+        <h4 style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>@Felipe</h4>
+        <div className='contact__icons-2 d-flex justify-content-evenly'>
+          <a className='link' href='https://github.com/Ariaqho' target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon></a>
+          <a className='link' href='https://linkedin.com/in/felipe-maldonado-nunez/' target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon></a>
+          <a className='link' href='mailto:felipe.arica@gmail.com' target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon></a>
+        </div>
+      </div>
     </footer>
   )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,15 @@
-// import logoWizard from './wizard-icon.jpg';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Nav, Navbar } from 'react-bootstrap';
 import { Outlet, Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHatWizard } from '@fortawesome/free-solid-svg-icons';
 
 function NavbarComponent() {
   return (
     <>
       <Navbar bg='dark' variant = 'dark' sticky='top' expand='lg' collapseOnSelect>
-          <Navbar.Brand>
-              {/* <img src={logoWizard} width='40px' heigth='40px' alt='wizard hat'/>{''} */}
-              <Nav.Link href='/'>Potter</Nav.Link>
+          <Navbar.Brand className='logo-brand'>          
+              <Nav.Link href='/'><FontAwesomeIcon icon={faHatWizard} className='icon-hat'></FontAwesomeIcon>Potter</Nav.Link>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
@@ -17,8 +17,6 @@ function NavbarComponent() {
               <Nav.Link href='hechizos'>Hechizos</Nav.Link>
               <Nav.Link href='personajes'>Personajes</Nav.Link>
               <Nav.Link href='libros'>Libros</Nav.Link>
-
-              {/* <Link to="/personajes">Personajes</Link> */}
           </Nav>
           </Navbar.Collapse>
           
