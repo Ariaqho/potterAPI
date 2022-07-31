54dffc1a55bfccb5e5e28dd928210e7a185624d9
Felipe Maldonado <felipe.arica@gmail.com>
2022-07-31 02:50:22 -0400

    navbar responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-// import wand from './img/wand.svg';
+// import logoWizard from './wizard-icon.jpg';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Nav, Navbar } from 'react-bootstrap';
 import { Outlet, Link } from "react-router-dom";
@@ -6,18 +6,22 @@ import { Outlet, Link } from "react-router-dom";
 function NavbarComponent() {
   return (
     <>
-      <Navbar bg='dark' variant = 'dark' sticky='top'>
+      <Navbar bg='dark' variant = 'dark' sticky='top' expand='lg' collapseOnSelect>
           <Navbar.Brand>
-              {/* <img src= width='40px' heigth='40px' />{''} */}
+              {/* <img src={logoWizard} width='40px' heigth='40px' alt='wizard hat'/>{''} */}
               <Nav.Link href='/'>Potter</Nav.Link>
           </Navbar.Brand>
-          <Nav>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
               <Nav.Link href='hechizos'>Hechizos</Nav.Link>
               <Nav.Link href='personajes'>Personajes</Nav.Link>
               <Nav.Link href='libros'>Libros</Nav.Link>
 
               {/* <Link to="/personajes">Personajes</Link> */}
           </Nav>
+          </Navbar.Collapse>
+          
       </Navbar>
       <Outlet />
 
